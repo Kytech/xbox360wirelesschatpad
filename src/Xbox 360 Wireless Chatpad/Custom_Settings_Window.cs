fb9b59a2d33781ea5a7609e6c415bdaab6d5ec6f
Kytech <Kytech@users.noreply.github.com>
2014-06-19 00:32:12 +0000

    Added Additional GUI Elements and Dialogs (Custom_Settings_Window.cs) for the use with custom button mappings. Found a bug where trigger as button settings and Custom button mapping settings don't always read out of the config file.

---
+++ src/Xbox 360 Wireless Chatpad/Custom_Settings_Window.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Xbox360WirelessChatpad
+{
+    public partial class Custom_Settings_Window : Form
+    {
+        //stores the value of the controller number being configured
+        private int controllerNumber;
+
+        //stores the path for the controller profile
+        public string profilePath;
+
+        public Custom_Settings_Window(int ctrlNum)
+        {
+            InitializeComponent();
+            this.controllerNumber = ctrlNum;
+        }
+
+        private void Custom_Settings_Window_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void profileSelector_FileOk(object sender, CancelEventArgs e)
+        {
+            filePath.Text = profileSelector.FileName;
+            profileSelector.FileName = this.profilePath;
+            profileSelector.Dispose();
+        }
+
+        private void browse_Click(object sender, EventArgs e)
+        {
+            string cd = Environment.CurrentDirectory;
+            if (Directory.Exists(cd + @"\Profiles") == false)
+            {
+                Directory.CreateDirectory(cd + @"Profiles");
+            }
+            profileSelector.InitialDirectory = cd + @"\Profiles";
+            profileSelector.ShowDialog();
+        }
+    }
+}
